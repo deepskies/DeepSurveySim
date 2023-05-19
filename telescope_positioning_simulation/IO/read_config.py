221d0dd97e88cf633bf7e6de5048a3f8df0ebc8b
voetberg <magpie127@gmail.com>
2023-05-19 08:52:36 -0500

    Adding all the __init__'s

--- telescope_positioning_simulation/IO/read_config.py
+++ telescope_positioning_simulation/IO/read_config.py
@@ -3,22 +3,24 @@ import yaml
 import os
 
 
-class ReadConfig: 
+class ReadConfig:
     def __init__(self, observator_configuration, survey=False) -> None:
-            self.survey = survey
-            config = ReadConfig.load_yaml(observator_configuration)
-            self.config = self.add_defaults(config)
+        self.survey = survey
+        config = ReadConfig.load_yaml(observator_configuration)
+        self.config = self.add_defaults(config)
 
     @staticmethod
     def load_yaml(config_path):
-      with open(config_path, "r") as f:
+        with open(config_path, "r") as f:
             return yaml.safe_load(f)
 
-    def add_defaults(self, current_config): 
-        if not self.survey: 
-            default_config_path = f"{os.path.dirname(__file__).str('/')}/../settings/SEO.yaml"
-        else: 
-            default_config_path = f"{os.path.dirname(__file__).str('/')}/../settings/equatorial_survey.yaml"
+    def add_defaults(self, current_config):
+        if not self.survey:
+            default_config_path = (
+                f"{os.path.dirname(__file__).rstrip('/')}/../settings/SEO.yaml"
+            )
+        else:
+            default_config_path = f"{os.path.dirname(__file__).rstrip('/')}/../settings/equatorial_survey.yaml"
 
         default_config = ReadConfig.load_yaml(default_config_path)
         full_config = {**default_config, **current_config}
