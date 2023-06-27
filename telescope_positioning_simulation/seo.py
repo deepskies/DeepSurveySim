95a9404e00d351077c7b6e326a2773ad2155c988
terranovaa <terranovafrplus@gmail.com>
2023-06-27 15:26:51 -0500

    Added model_tune and requirements file. Adapted seo.py to new changes.

--- telescope_positioning_simulation/seo.py
+++ telescope_positioning_simulation/seo.py
@@ -1,28 +1,34 @@
-from Survey.RLLib_survey import Survey
+from Survey.survey import Survey
 from IO.read_config import ReadConfig
 from astropy.time import Time
+import numpy as np
+import random
 
 seo_config = ReadConfig(
         observator_configuration="settings/SEO.yaml"
     )()
 
+
 survey_config = ReadConfig(
         observator_configuration="settings/equatorial_survey.yaml",
         survey=True
     )()
-
-survey_config['location']  = {'ra': [0], 'decl':[0]}
+survey_config['location']  = {'ra': 0, 'decl':0}
 
 env = Survey(seo_config, survey_config)
-observation = env.reset()
+observation = env._observation_calculation()
 
 stop = False
 while not stop:
+    ra = random.randint(0, 90)
+    decl = random.randint(0, 90)
     action = {
         "time": Time.now().mjd,
-        "ra": [50],
-        "decl": [50],
+        "location": {"ra": ra, "decl": decl},
         "band": "g"
     }
-    observation, reward, stop, truncation, log = env.step(action)
-    print(observation, reward, stop, truncation, log)
+    print("Action",action)
+    observation, reward, stop, log = env.step(action)
+    print("Observation",observation)
+    print("Reward",reward)
+    print("Stop",stop)
