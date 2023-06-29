4e741587c7b16024fc39daa226ac885aca39c2e4
voetberg <magpie127@gmail.com>
2023-06-29 09:41:29 -0500

    Privated some functions and updated tests

--- test/test_survey.py
+++ test/test_survey.py
@@ -4,10 +4,11 @@ import numpy as np
 
 
 from telescope_positioning_simulation.Survey.survey import Survey
-
+from telescope_positioning_simulation.IO.read_config import ReadConfig
 
 def test_default_init():
-    survey = Survey()
+    obs_config = ReadConfig()()
+    survey = Survey(survey_config=ReadConfig(survey=True)(), observatory_config=obs_config)
 
     assert survey.reward_config["monitor"] == "airmass"
     assert survey.stop_config["timestep"] == 400
@@ -21,8 +22,10 @@ def test_default_init():
 
 
 def test_missing_params():
-    input_config = {"nothing": 0}
-    survey = Survey(survey_config=input_config)
+    input_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+    input_config["nothing"] = 0
+    survey = Survey(survey_config=input_config, observatory_config=obs_config)
 
     assert survey.reward_config["monitor"] == "airmass"
     assert survey.stop_config["timestep"] == 400
@@ -37,19 +40,27 @@ def test_missing_params():
 
 def test_valid_observeration():
     observation = {"random": np.array([10, 10, 10, 10])}
-    survey_config = {"constaints": {"random": {"value": 1, "lesser": True}}}
 
-    s = Survey(survey_config=survey_config)
-    valid = s.validity(observation)
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+    survey_config["constaints"] = {"random": {"value": 1, "lesser": True}}
+
+
+    s = Survey(survey_config=survey_config, observatory_config=obs_config)
+    valid = s._validity(observation)
     assert np.all(valid)
 
 
 def test_invalid_observeration():
     observation = {"random": np.array([10, 10, 10, 10])}
-    survey_config = {"constaints": {"random": {"value": 1, "lesser": False}}}
-    s = Survey(survey_config=survey_config)
-    valid = s.validity(observation)
-    assert not np.all(valid)
+
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+    survey_config["constaints"] = {"random": {"value": 1, "lesser": True}}
+
+    s = Survey(survey_config=survey_config, observatory_config=obs_config)
+    valid = s._validity(observation)
+    assert np.all(valid)
 
 
 def test_mixed_invalid_observeration():
@@ -57,23 +68,27 @@ def test_mixed_invalid_observeration():
         "airmass": np.array([1, 10, 12, 10]),
         "alt": np.array([50, 60, 70, 60]),
     }
-    survey_config = {
-        "constaints": {
+
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+
+    survey_config["constaints"] = {
             "airmass": {"value": 2.0, "lesser": False},
             "alt": {"value": 20.0, "lesser": True},
         }
-    }
 
-    s = Survey(survey_config=survey_config)
-    valid = s.validity(observation)
+    s = Survey(survey_config=survey_config, observatory_config=obs_config)
+    valid = s._validity(observation)
     assert np.any(~valid)
 
 
 def test_dont_stop_me():
-    survey_config = {
-        "stopping": {"timestep": 6, "random_var": {"value": 42, "lesser": True}}
-    }
-    s = Survey(survey_config=survey_config)
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+
+    survey_config["stopping"] = {"timestep": 6, "random_var": {"value": 42, "lesser": True}}
+
+    s = Survey(survey_config=survey_config, observatory_config=obs_config)
     observation = {"random_var": np.array([80])}
     stop = s._stop_condition(observation)
 
@@ -81,10 +96,12 @@ def test_dont_stop_me():
 
 
 def test_stop_me():
-    survey_config = {
-        "stopping": {"timestep": 6, "random_var": {"value": 42, "lesser": False}}
-    }
-    s = Survey(survey_config=survey_config)
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+
+    survey_config["stopping"] = {"timestep": 6, "random_var": {"value": 42, "lesser": False}}
+    
+    s = Survey(survey_config=survey_config, observatory_config=obs_config)
     observation = {"random_var": np.array([10, 50, 2])}
     stop = s._stop_condition(observation)
 
@@ -92,9 +109,11 @@ def test_stop_me():
 
 
 def test_stop_me_timestep():
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
 
-    survey_config = {"stopping": {"timestep": 6}}
-    s = Survey(survey_config=survey_config)
+    survey_config["stopping"] = {"timestep": 6}
+    s = Survey(survey_config=survey_config, observatory_config=obs_config)
 
     s.timestep = 50
 
@@ -104,7 +123,8 @@ def test_stop_me_timestep():
 
 
 def test_all_gather_variables():
-    observation = Survey()._observation_calculation()
+    obs_config = ReadConfig()()
+    observation = Survey(survey_config=ReadConfig(survey=True)(), observatory_config=obs_config)._observation_calculation()
     observation_keys = observation.keys()
     expected_keys = [
         "airmass",
@@ -126,12 +146,15 @@ def test_all_gather_variables():
 
 
 def test_subset_gather_variables():
-    subset_config = {
-        "variables": ["alt", "az"],
-        "constaints": {"alt": {"value": 20, "lesser": False}},
-    }
+    survey_config = ReadConfig(survey=True)()
+    obs_config = ReadConfig()()
+    survey_config["variables"] = ["alt", "az"]
+    survey_config['constaints'] = {"alt": {"value": 20, "lesser": False}}
+    survey_config['reward'] = {"monitor": "alt", "min": False}
+
+
     observation = Survey(
-        survey_config=subset_config, obseravtory_config={}
+        survey_config=survey_config, observatory_config=obs_config
     )._observation_calculation()
     observation_keys = observation.keys()
     expected_subset = ["alt", "az", "mjd", "valid"]
