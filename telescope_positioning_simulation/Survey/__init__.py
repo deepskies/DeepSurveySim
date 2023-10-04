2190b4c0c7afadc62ae03d54b7527abdebdf4f9d
voetberg <maggiev@fnal.gov>
2023-10-04 09:09:04 -0500

    Delete telescope_positioning_simulation directory
    repeated code

--- telescope_positioning_simulation/Survey/__init__.py
+++
@@ -1,9 +0,0 @@
-from telescope_positioning_simulation.Survey.survey import Survey
-from telescope_positioning_simulation.Survey.observation_variables import (
-    ObservationVariables,
-)
-from telescope_positioning_simulation.Survey.weather import Weather
-from telescope_positioning_simulation.Survey.cummulative_survey import (
-    UniformSurvey,
-    LowVisiblitySurvey,
-)
